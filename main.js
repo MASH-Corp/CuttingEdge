030c1ce8bf1e93c9057edb2678318b2994071669
premier <bdm@mash-corp.com>
2025-09-14 01:24:53 +0200

    ar 13

--- main.js
+++ main.js
@@ -4,42 +4,42 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 // Camera Access
-    const video = document.getElementById('camera');
+    // const video = document.getElementById('camera');
 
-    // Ask for camera access
-    navigator.mediaDevices.getUserMedia(
-        { video: { facingMode: { exact: "environment" } },
-         audio: false 
-        })
-      .then(stream => {
-        // Set the video srcObject to the camera stream
-        video.srcObject = stream;
-      })
-      .catch(error => {
-        console.error("Error accessing camera:", error);
-      });
+    // // Ask for camera access
+    // navigator.mediaDevices.getUserMedia(
+    //     { video: { facingMode: { exact: "environment" } },
+    //      audio: false 
+    //     })
+    //   .then(stream => {
+    //     // Set the video srcObject to the camera stream
+    //     video.srcObject = stream;
+    //   })
+    //   .catch(error => {
+    //     console.error("Error accessing camera:", error);
+    //   });
     
 // Info Icon Alert
-    const infoBox = document.getElementById('info');
-    const infoModal = document.getElementById('info-modal');
-    const closeBtn = document.getElementById('close-btn');
+    // const infoBox = document.getElementById('info');
+    // const infoModal = document.getElementById('info-modal');
+    // const closeBtn = document.getElementById('close-btn');
 
-    infoBox.addEventListener('click', () => {
-      infoModal.style.display = 'flex';
+    // infoBox.addEventListener('click', () => {
+    //   infoModal.style.display = 'flex';
         
-    });
+    // });
 
-    closeBtn.addEventListener('click', () => {
-      infoModal.style.display = 'none';
-    });
+    // closeBtn.addEventListener('click', () => {
+    //   infoModal.style.display = 'none';
+    // });
 
     // size
-    const modelViewer = document.getElementById('modelViewer');
-      const scaleSlider = document.getElementById('model-scale');
-      scaleSlider.addEventListener('input', () => {
-        const scale = scaleSlider.value;
-        modelViewer.scale = `${scale} ${scale} ${scale}`;
-      });
+    // const modelViewer = document.getElementById('modelViewer');
+    //   const scaleSlider = document.getElementById('model-scale');
+    //   scaleSlider.addEventListener('input', () => {
+    //     const scale = scaleSlider.value;
+    //     modelViewer.scale = `${scale} ${scale} ${scale}`;
+    //   });
 
       // Three.js + AR Setup
     let camera, scene, renderer, model;
