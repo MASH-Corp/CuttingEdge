33b9a24a82637f2d2d7ac4e95faedf4ed6f24096
premier <bdm@mash-corp.com>
2025-09-13 14:58:51 +0200

    country 1

---
+++ main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+// Camera Access
+    const video = document.getElementById('camera');
+
+    // Ask for camera access
+    navigator.mediaDevices.getUserMedia(
+        { video: { facingMode: { exact: "environment" } },
+         audio: false 
+        })
+      .then(stream => {
+        // Set the video srcObject to the camera stream
+        video.srcObject = stream;
+      })
+      .catch(error => {
+        console.error("Error accessing camera:", error);
+      });
+    
+// Info Icon Alert
+const infoBox = document.getElementById('info');
+const infoModal = document.getElementById('info-modal');
+const closeBtn = document.getElementById('close-btn');
+
+infoBox.addEventListener('click', () => {
+  infoModal.style.display = 'flex';
+    
+});
+
+closeBtn.addEventListener('click', () => {
+  infoModal.style.display = 'none';
+});
+
+});
